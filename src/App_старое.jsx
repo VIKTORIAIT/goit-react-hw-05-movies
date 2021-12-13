112afdd4f952eefa2d0627fc3ca5ed831f8771aa
VIKTORIAIT <89775075+VIKTORIAIT@users.noreply.github.com>
2021-12-13 20:50:48 +0200

     add

--- src/App_старое.jsx
+++ src/App_старое.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import { Component } from "react";
 import Loader from "./components/Loader/Loader";
 import Searchbar from "./components/Searchbar/Searchbar";
@@ -8,18 +9,17 @@ import { Modal } from "./components/Modal/Modal";
 import s from "./App.module.css";
 
 const API_KEY = "24365762-4d41dfacdb025e40bdae241c8";
-class App extends Component {
-  state = {
-    hits: [],
-    totalHits: null,
-    page: 1,
-    q: "",
-    isLoad: false,
-    id: null,
-    isModalOpen: false,
-  };
 
-  fetchRequest() {
+export default function App() {
+  const [hits, setHits] = useState([]);
+  const [totalHits, setTotalHits] = useState(null);
+  const [page, setPage] = useState(1);
+  const [q, setQ] = useState("");
+  const [isLoad, setIsLoad] = useState(false);
+  const [id, setId] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const fetchRequest = () => {
     const fetchParams = {
       per_page: 12,
       orientation: "horizontal",
@@ -47,23 +47,23 @@ class App extends Component {
       .catch((error) => {
         this.setState({ isLoad: false });
       });
-  }
+  };
 
-  componentDidMount() {
+  const componentDidMount = () => {
     this.fetchRequest();
-  }
+  };
 
-  componentDidUpdate(prevProps, prevState) {
+  const componentDidUpdate = (prevProps, prevState) => {
     if (this.state.q !== prevState.q || this.state.page !== prevState.page) {
       this.fetchRequest();
     }
-  }
+  };
 
-  componentWillUnmount() {
+  const componentWillUnmount = () => {
     console.log("unmount");
-  }
+  };
 
-  onSubmit = (ev) => {
+  const onSubmit = (ev) => {
     ev.preventDefault();
     console.log(ev.target.search.value);
 
@@ -72,58 +72,54 @@ class App extends Component {
       q: ev.target.search.value,
     });
   };
-  onMoreButtonClick = () => {
+  const onMoreButtonClick = () => {
     this.setState((prevState) => ({
       page: prevState.page + 1,
     }));
   };
 
-  onImageClick = (ev) => {
+  const onImageClick = (ev) => {
     if (ev.target.tagName === "IMG") {
       this.setState({ id: ev.target.id });
     }
     this.onModalOpen();
   };
 
-  onModalOpen = () => {
+  const onModalOpen = () => {
     this.setState({ isModalOpen: true });
   };
 
-  handleEscape = (ev) => {
+  const handleEscape = (ev) => {
     console.log(ev.code);
     if (ev.code === "Escape") {
       this.setState({ isModalOpen: false });
     }
   };
 
-  onModalClose = (ev) => {
+  const onModalClose = (ev) => {
     if (ev.target.tagName === "IMG") return;
     this.setState({ isModalOpen: false });
   };
 
-  render() {
-    const { hits, isLoad, isModalOpen, id, totalHits, page } = this.state;
-    const maxPage = Math.ceil(totalHits / 12) === page;
-    return (
-      <div className={s.App}>
-        <Searchbar onSubmit={this.onSubmit} />
-        {this.state.isLoad && <Loader />}
-        <ImageGallery imgs={hits} onClick={this.onImageClick} />
-        {!isLoad && !maxPage && totalHits !== 0 && (
-          <Button onClick={this.onMoreButtonClick} />
-        )}
-        {this.state.isLoad && page !== 1 && <Loader />}
-        {isModalOpen && (
-          <Modal
-            handleEscape={this.handleEscape}
-            imgs={hits}
-            id={id}
-            onClick={this.onModalClose}
-          />
-        )}
-      </div>
-    );
-  }
+  const { hits, isLoad, isModalOpen, id, totalHits, page } = this.state;
+  const maxPage = Math.ceil(totalHits / 12) === page;
+  return (
+    <div className={s.App}>
+      <Searchbar onSubmit={this.onSubmit} />
+      {this.state.isLoad && <Loader />}
+      <ImageGallery imgs={hits} onClick={onImageClick} />
+      {!isLoad && !maxPage && totalHits !== 0 && (
+        <Button onClick={onMoreButtonClick} />
+      )}
+      {this.state.isLoad && page !== 1 && <Loader />}
+      {isModalOpen && (
+        <Modal
+          handleEscape={handleEscape}
+          imgs={hits}
+          id={id}
+          onClick={onModalClose}
+        />
+      )}
+    </div>
+  );
 }
-
-export default App;
